f80faa5e2272989ff0db34b686771d7e4c5871c2
Mathias <108764032+mathip94@users.noreply.github.com>
2025-11-19 17:21:37 -0300

    Create variables.tf

---
+++ iac/modules/compute/variables.tf
@@ -0,0 +1,36 @@
+variable "project_name" {
+  type = string
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "ecs_sg_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "asg_min_size" {
+  type    = number
+  default = 1
+}
+
+variable "asg_max_size" {
+  type    = number
+  default = 2
+}
+
+variable "asg_desired_capacity" {
+  type    = number
+  default = 1
+}
+
+variable "instance_profile_name" {
+  description = "Name of the IAM Instance Profile"
+  type        = string
+}
