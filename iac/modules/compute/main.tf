8db7aed5cf74f95210851ed66c24de99a70e4c2f
Mathias <108764032+mathip94@users.noreply.github.com>
2025-11-19 17:11:27 -0300

    Create main.tf
    ECS cluster main.tf

---
+++ iac/modules/compute/main.tf
@@ -0,0 +1,81 @@
+resource "aws_ecs_cluster" "main" {
+  name = "${var.project_name}-cluster"
+}
+
+resource "aws_ecs_cluster_capacity_providers" "main" {
+  cluster_name = aws_ecs_cluster.main.name
+
+  capacity_providers = [aws_ecs_capacity_provider.main.name]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = aws_ecs_capacity_provider.main.name
+  }
+}
+
+resource "aws_ecs_capacity_provider" "main" {
+  name = "${var.project_name}-capacity-provider"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn = aws_autoscaling_group.main.arn
+
+    managed_scaling {
+      maximum_scaling_step_size = 1000
+      minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity           = 100
+    }
+  }
+}
+
+resource "aws_autoscaling_group" "main" {
+  name                = "${var.project_name}-asg"
+  vpc_zone_identifier = var.subnet_ids
+  min_size            = var.asg_min_size
+  max_size            = var.asg_max_size
+  desired_capacity    = var.asg_desired_capacity
+
+  launch_template {
+    id      = aws_launch_template.main.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "AmazonECSManaged"
+    value               = true
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_launch_template" "main" {
+  name_prefix   = "${var.project_name}-lt-"
+  image_id      = data.aws_ssm_parameter.ecs_optimized_ami.value
+  instance_type = var.instance_type
+
+  iam_instance_profile {
+    name = var.instance_profile_name
+  }
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [var.ecs_sg_id]
+  }
+
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              echo ECS_CLUSTER=${aws_ecs_cluster.main.name} >> /etc/ecs/ecs.config
+              EOF
+  )
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "${var.project_name}-ecs-node"
+    }
+  }
+}
+
+data "aws_ssm_parameter" "ecs_optimized_ami" {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+}
