727540b6b88bea58b79fe23a0e2beee26bc9579f
Mathias <108764032+mathip94@users.noreply.github.com>
2025-11-19 17:23:44 -0300

    Create variables.tf

---
+++ iac/modules/networking/variables.tf
@@ -0,0 +1,22 @@
+variable "project_name" {
+  description = "Project name prefix"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDRs for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of AZs to use"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
