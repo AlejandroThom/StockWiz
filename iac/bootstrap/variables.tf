30b274a4e2235e03003732bac9f038a6bd65e504
Mathias <108764032+mathip94@users.noreply.github.com>
2025-11-19 16:53:47 -0300

    Create variables.tf

---
+++ iac/bootstrap/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket for Terraform state"
+  type        = string
+  default     = "obligatorio-devops-tf-state"
+}
+
+variable "dynamodb_table_name" {
+  description = "Name of the DynamoDB table for state locking"
+  type        = string
+  default     = "obligatorio-devops-tf-locks"
+}
