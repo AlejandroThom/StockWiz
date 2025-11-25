64fb0e8ae1dddf620d211677918405524253d255
Mathias <108764032+mathip94@users.noreply.github.com>
2025-11-24 21:49:07 -0300

    Update variables.tf

--- iac/bootstrap/variables.tf
+++ iac/bootstrap/variables.tf
@@ -7,11 +7,11 @@ variable "aws_region" {
 variable "bucket_name" {
   description = "Name of the S3 bucket for Terraform state"
   type        = string
-  default     = "obligatorio-devops-tf-state"
+  default     = "obligatorio-devops-tf-state-123"
 }
 
 variable "dynamodb_table_name" {
   description = "Name of the DynamoDB table for state locking"
   type        = string
-  default     = "obligatorio-devops-tf-locks"
+  default     = "obligatorio-devops-tf-locks-123"
 }
