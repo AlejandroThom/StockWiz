74949ffa933868f12ecfaaa2f94b80e5b563b0c7
LuisThom <theicehornet1@gmail.com>
2025-11-30 19:18:19 -0300

    feat: Initialize dev environment infrastructure with core AWS services using Terraform modules.

--- iac/environments/dev/main.tf
+++ iac/environments/dev/main.tf
@@ -15,7 +15,8 @@ terraform {
 module "networking" {
   source       = "../../modules/networking"
   project_name = var.project_name
-  vpc_cidr     = "10.0.0.0/16"
+  vpc_cidr            = "10.0.0.0/16"
+  public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 module "security" {
