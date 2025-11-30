5f714b12cd4346aee7c47ceba814e3bfde14d285
LuisThom <theicehornet1@gmail.com>
2025-11-30 16:48:40 -0300

    fix: update default CIDR values for public subnets in networking module

--- iac/modules/networking/variables.tf
+++ iac/modules/networking/variables.tf
@@ -12,7 +12,7 @@ variable "vpc_cidr" {
 variable "public_subnet_cidrs" {
   description = "List of CIDRs for public subnets"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  default     = ["10.1.1.0/24", "10.1.2.0/24"]
 }
 
 variable "availability_zones" {
